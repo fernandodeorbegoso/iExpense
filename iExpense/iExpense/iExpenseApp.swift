427ea64797f6aebf453fcc457df7021d137cccde
fernandodeorbegoso <fernando@fdoenterprises.com>
2023-02-21 08:46:15 -0500

    first commit

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Fernando de Orbegoso on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
